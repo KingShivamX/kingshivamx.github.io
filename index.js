8c0238772ffd1f6d91bb1f8a6c6111bb38e9e84e
Shivam Hippalgave <142880709+KingShivamX@users.noreply.github.com>
2024-04-21 19:53:33 +0530

    dice game change

---
+++ index.js
@@ -0,0 +1,28 @@
+// ///////////////////////////////////////////////////////////////////////////
+var randomNumber1 = Math.floor((Math.random() * 6)+1);
+
+var diceimg1 = "./images/dice"+randomNumber1+".png"
+
+var dice1 = document.querySelector(".img1");
+
+dice1.setAttribute("src", diceimg1);
+// ////////////////////////////////////////////////////////////////////////////
+var randomNumber2 = Math.floor((Math.random() * 6)+1);
+
+var diceimg2 = "./images/dice"+randomNumber2+".png"
+
+var dice2 = document.querySelector(".img2");
+
+dice2.setAttribute("src", diceimg2);
+// //////////////////////////////////////////////////////////////////////////
+function winner (num1, num2){
+    if (num1 < num2) {
+        return document.querySelector(".win").textContent="Player 2 Won"
+    } else if (num1 > num2) {
+        return document.querySelector(".win").textContent="Player 1 Won"
+    } else {
+        return document.querySelector(".win").textContent="Its a Draw"
+    }
+}
+
+winner(randomNumber1, randomNumber2);
