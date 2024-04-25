87b1822391a4c630c6e39430bc86065895585bee
Shivam Hippalgave <142880709+KingShivamX@users.noreply.github.com>
2024-04-26 03:21:12 +0530

    Add files via upload

---
+++ DiceGame/index.js
@@ -0,0 +1,41 @@
+// ///////////////////////////////////////////////////////////////////////////
+var randomNumber1 = Math.floor((Math.random() * 6)+1);
+
+var diceimg1 = "./images/dice"+randomNumber1+".png"
+
+var dice1 = document.querySelector(".img1");
+
+dice1.setAttribute("src", diceimg1);
+// ////////////////////////////////////////////////////////////////////////////
+var randomNumber2 = Math.floor((Math.random() * 6)+1);
+
+var diceimg2 = "./images/dice"+randomNumber2+".png"
+
+var dice2 = document.querySelector(".img2");
+
+dice2.setAttribute("src", diceimg2);
+// //////////////////////////////////////////////////////////////////////////
+function winner (num1, num2){
+    if (num1 < num2) {
+        return document.querySelector(".win").textContent="Player 2 Won"
+    } else if (num1 > num2) {
+        return document.querySelector(".win").textContent="Player 1 Won"
+    } else {
+        return document.querySelector(".win").textContent="Its a Draw"
+    }
+}
+winner(randomNumber1, randomNumber2);
+
+function playsound() {
+    var audio = new Audio("./images/zDiceSound.mp3");
+    audio.play();
+}
+
+//// to refresh page , play again button 
+document.querySelector(".reee").addEventListener("click",function() {
+    // location.reload();
+    playsound();
+    setTimeout(function() {
+        location.reload();
+    }, 800);
+})
